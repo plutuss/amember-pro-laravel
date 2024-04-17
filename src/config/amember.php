3f1357f1a69f615ece8ce4d5d7a07b3ee77c8a3b
plutuss <dima92konovalenko@gmail.com>
2024-04-17 16:26:13 +0200

    change the response class

--- src/config/amember.php
+++ src/config/amember.php
@@ -1,17 +1,33 @@
 <?php
 
+use Plutuss\AMember\Response\Adapter\ArrayReportAdapter;
+use Plutuss\AMember\Response\Adapter\CollectionReportAdapter;
+use Plutuss\AMember\Response\Adapter\JsonReportAdapter;
+
 return [
+
+
+    'amember_url' => env('AMEMBER_URL', ''),
+    'amember_api_key' => env('AMEMBER_API_KEY', ''),
 
     'type_response' => [
 
         /**
+         * You can choose the format of your answer
+         *
          * collection, array, json
          */
-
         'default' => env('AMEMBER_TYPE_RESPONSE', 'collection'),
+
+        /**
+         * You can change the response class by using ReportAdapterInterface in your class
+         */
+        'response_class' => [
+            'collection' => CollectionReportAdapter::class,
+            'array' => ArrayReportAdapter::class,
+            'json' => JsonReportAdapter::class,
+        ],
 
     ],
 
-    'amember_url' => env('AMEMBER_URL', ''),
-    'amember_api_key' => env('AMEMBER_API_KEY', ''),
 ];
