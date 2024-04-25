70500b3bc9206bdc9edec202e58296bf559bab76
plutuss <dima92konovalenko@gmail.com>
2024-04-25 12:03:17 +0200

    update: add endpoint

--- src/Services/Access/AMemberAccessService.php
+++ src/Services/Access/AMemberAccessService.php
@@ -2,6 +2,7 @@
 
 namespace Plutuss\AMember\Services\Access;
 
+use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Collection;
 use Plutuss\AMember\Clients\AMemberClient;
@@ -13,6 +14,7 @@ class AMemberAccessService extends AMemberClient implements AMemberAccessInterfa
     /**
      * @param int|null $id
      * @return JsonResponse|array|Collection
+     * @throws ConnectionException
      */
     public function getAccess(?int $id = null): JsonResponse|array|Collection
     {
@@ -21,14 +23,41 @@ class AMemberAccessService extends AMemberClient implements AMemberAccessInterfa
         )->sendGet();
     }
 
+
     /**
      * @param int|null $id
      * @return JsonResponse|array|Collection
+     * @throws ConnectionException
      */
     public function getAccessLog(?int $id = null): JsonResponse|array|Collection
     {
         return $this->setOption(
             url_join('/access-log', $id)
         )->sendGet();
+    }
+
+    /**
+     * @param array $data
+     * @return JsonResponse|array|Collection
+     * @throws ConnectionException
+     */
+    public function setAccess(array $data): JsonResponse|array|Collection
+    {
+        return $this->setOption(
+            url_join('/access'), $data
+        )->sendPost();
+    }
+
+    /**
+     * @param int $id
+     * @param array $data
+     * @return JsonResponse|array|Collection
+     * @throws ConnectionException
+     */
+    public function updateAccess(int $id, array $data): JsonResponse|array|Collection
+    {
+        return $this->setOption(
+            url_join('/access', $id), $data
+        )->sendPut();
     }
 }
